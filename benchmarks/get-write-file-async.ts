71e9b245905929d369f41daa7c7cdd55b3de0c7b
howmanysmall <26746527+howmanysmall@users.noreply.github.com>
2025-11-09 20:36:09 -0700

    chore: remove knip and related dependencies, add valibot

---
+++ benchmarks/get-write-file-async.ts
@@ -0,0 +1,25 @@
+export type WriteFileAsync = (filePath: string, content: string | Uint8Array) => Promise<void>;
+
+interface DenoLike {
+	readonly writeTextFile: (filePath: string, content: string) => Promise<void>;
+}
+
+export default async function getWriteFileAsync(): Promise<WriteFileAsync> {
+	if (globalThis.Bun !== undefined) {
+		return async function writeFileAsync(path: string, content: string | Uint8Array): Promise<void> {
+			await Bun.write(path, content);
+		};
+	}
+
+	const { Deno } = globalThis as typeof globalThis & { Deno?: DenoLike };
+	if (Deno !== undefined) {
+		return async function writeFileAsync(path: string, content: string | Uint8Array): Promise<void> {
+			await Deno.writeTextFile(path, typeof content === "string" ? content : new TextDecoder().decode(content));
+		};
+	}
+
+	const fs = await import("node:fs/promises");
+	return async function writeFileAsync(path: string, content: string | Uint8Array): Promise<void> {
+		await fs.writeFile(path, content);
+	};
+}
