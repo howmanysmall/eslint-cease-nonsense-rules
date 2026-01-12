82fa10819e3d1c7d5df703ea9f4b8537db458754
howmanysmall <26746527+howmanysmall@users.noreply.github.com>
2026-01-11 19:30:30 -0700

    chore(git): setup commitlint and expand git hooks
    Introduce commitlint to enforce conventional
    commit messages. Update
    git hooks to automate dependency installation on
    merge and run type
    checks and tests before pushing.

--- .simple-git-hooks.js
+++ .simple-git-hooks.js
@@ -1,5 +1,8 @@
 const simpleGitHooks = {
+	"commit-msg": 'bun x --bun commitlint --edit "$1"',
+	"post-merge": "bun install",
 	"pre-commit": "bun run lint-staged",
+	"pre-push": "bun run type-check && bun run test",
 };
 
 export default simpleGitHooks;
