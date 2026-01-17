dfb05d36b7e540493989f09a16ed8284d5f514fd
howmanysmall <26746527+howmanysmall@users.noreply.github.com>
2026-01-17 02:10:31 -0700

    chore(hooks): update pre-push hook to run lint instead of test
    Update the pre-push hook to run lint instead of test and add a type
    annotation for the configuration.

--- .simple-git-hooks.js
+++ .simple-git-hooks.js
@@ -1,8 +1,11 @@
+/**
+ * @type {import('simple-git-hooks').SimpleGitHooks}
+ */
 const simpleGitHooks = {
 	"commit-msg": 'bun x --bun commitlint --edit "$1"',
 	"post-merge": "bun install",
 	"pre-commit": "bun run lint-staged",
-	"pre-push": "bun run type-check && bun run test",
+	"pre-push": "bun run type-check && bun run lint",
 };
 
 export default simpleGitHooks;
