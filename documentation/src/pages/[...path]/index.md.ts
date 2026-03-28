8963e7bc6ee5708c835dcbbf94901b33360b23f1
howmanysmall <26746527+howmanysmall@users.noreply.github.com>
2026-03-27 21:48:20 -0600

    refactor(docs): add explicit collection entry types to path generator

--- documentation/src/pages/[...path]/index.md.ts
+++ documentation/src/pages/[...path]/index.md.ts
@@ -3,11 +3,12 @@ import { getCollection } from "astro:content";
 import { createMarkdownResponse } from "../../utilities/create-markdown-response";
 
 import type { APIRoute, GetStaticPaths } from "astro";
+import type { CollectionEntry } from "astro:content";
 
 export const GET: APIRoute = async ({ params }) => createMarkdownResponse(params.path);
 
 export const getStaticPaths: GetStaticPaths = async () => {
-	const docs = await getCollection("docs");
+	const docs: Array<CollectionEntry<"docs">> = await getCollection("docs");
 	return docs
 		.filter((doc) => doc.id !== "index")
 		.map((doc) => ({
