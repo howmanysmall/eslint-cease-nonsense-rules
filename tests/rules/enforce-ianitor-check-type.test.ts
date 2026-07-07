howmanysmall/eslint-cease-nonsense-rules

commit a2d8b70819217256d9a0eb10bbca58af8e75f560

feat: big coverage

src/rules/enforce-ianitor-check-type.ts
@@ -9,11 +9,6 @@ export interface ComplexityConfiguration {
 	readonly warnThreshold: number;
 }
 
-interface ComplexityCache {
-	readonly nodeCache: WeakMap<object, number>;
-	readonly visitedNodes: WeakSet<object>;
-}
-
 const DEFAULT_CONFIGURATION: ComplexityConfiguration = {
 	baseThreshold: 10,
 	errorThreshold: 25,
@@ -22,24 +17,24 @@ const DEFAULT_CONFIGURATION: ComplexityConfiguration = {
 	warnThreshold: 15,
 };
 
-function hasTypeAnnotationProperty(node: object): node is { typeAnnotation: unknown } {
-	return "typeAnnotation" in node;
+type ParameterWithOptionalTypeAnnotation = TSESTree.Parameter & {
+	readonly typeAnnotation?: TSESTree.TSTypeAnnotation | undefined;
+};
+
+function hasTypeAnnotation(node: TSESTree.Identifier): boolean {
+	return Boolean(node.typeAnnotation);
 }
 
-function hasTypeAnnotation(node: TSESTree.VariableDeclarator): boolean {
-	if (node.id.type !== TSESTree.AST_NODE_TYPES.Identifier) return false;
-	if (typeof node.id !== "object" || !hasTypeAnnotationProperty(node.id)) return false;
-	return Boolean(node.id.typeAnnotation);
+function getParameterTypeAnnotation(
+	parameter: ParameterWithOptionalTypeAnnotation,
+): TSESTree.TSTypeAnnotation | undefined {
+	return parameter.typeAnnotation;
 }
 
-function isIanitorValidator(node: {
-	type: string;
-	callee?: { type: string; object?: { type: string; name?: string } };
-}): boolean {
+function isIanitorValidator(node: TSESTree.CallExpression): boolean {
 	return (
-		node.type === TSESTree.AST_NODE_TYPES.CallExpression &&
-		node.callee?.type === TSESTree.AST_NODE_TYPES.MemberExpression &&
-		node.callee.object?.type === TSESTree.AST_NODE_TYPES.Identifier &&
+		node.callee.type === TSESTree.AST_NODE_TYPES.MemberExpression &&
+		node.callee.object.type === TSESTree.AST_NODE_TYPES.Identifier &&
 		node.callee.object.name === "Ianitor"
 	);
 }
@@ -105,16 +100,10 @@ function hasIanitorStaticType(typeAnnotation: TSESTree.TypeNode): boolean {
 	);
 }
 
-function calculateIanitorComplexity(node: {
-	readonly callee?: {
-		readonly type: string;
-		readonly property?: { type: string; name?: string };
-	};
-	readonly arguments?: ReadonlyArray<{ type?: string; properties?: Array<unknown> }>;
-}): number {
+function calculateIanitorComplexity(node: TSESTree.CallExpression): number {
 	const { callee } = node;
 	if (
-		callee?.type !== TSESTree.AST_NODE_TYPES.MemberExpression ||
+		callee.type !== TSESTree.AST_NODE_TYPES.MemberExpression ||
 		callee.property?.type !== TSESTree.AST_NODE_TYPES.Identifier
 	) {
 		return 0;
@@ -126,7 +115,7 @@ function calculateIanitorComplexity(node: {
 		case "strictInterface": {
 			const properties = node.arguments?.[0];
 			return properties?.type === TSESTree.AST_NODE_TYPES.ObjectExpression
-				? 10 + (properties.properties?.length ?? 0) * 3
+				? 10 + properties.properties.length * 3
 				: 0;
 		}
 
@@ -142,7 +131,7 @@ function calculateIanitorComplexity(node: {
 
 		case "union":
 		case "intersection":
-			return (node.arguments?.length ?? 0) * 2;
+			return node.arguments.length * 2;
 
 		case "string":
 		case "number":
@@ -161,10 +150,6 @@ const enforceIanitorCheckType = createRule<Options, MessageIds>({
 	create(context) {
 		const [rawOptions] = context.options;
 		const config: ComplexityConfiguration = { ...DEFAULT_CONFIGURATION, ...rawOptions };
-		const cache: ComplexityCache = {
-			nodeCache: new WeakMap(),
-			visitedNodes: new WeakSet(),
-		};
 		const ianitorStaticVariables = new Set<string>();
 		const depthMultiplierCache = new Map<number, number>();
 		const complexityCeiling = config.errorThreshold * 2;
@@ -172,7 +157,7 @@ const enforceIanitorCheckType = createRule<Options, MessageIds>({
 		function getDepthMultiplier(depth: number): number {
 			const cached = depthMultiplierCache.get(depth);
 			if (cached !== undefined) return cached;
-			const computed = Math.log2(depth + 1);
+			const computed = depth === 0 ? 1 : Math.log2(depth + 1);
 			depthMultiplierCache.set(depth, computed);
 			return computed;
 		}
@@ -183,14 +168,96 @@ const enforceIanitorCheckType = createRule<Options, MessageIds>({
 			return Math.min(nextScore, complexityCeiling);
 		}
 
+		function calculateTypeMembersComplexity(
+			members: ReadonlyArray<TSESTree.TypeElement>,
+			baseScore: number,
+			nextDepth: number,
+		): number {
+			let score = addScore(baseScore, members.length * 2);
+			for (const member of members) {
+				const typeAnnotation = "typeAnnotation" in member ? member.typeAnnotation : undefined;
+				if (typeAnnotation !== undefined) {
+					score = addScore(score, calculateStructuralComplexity(typeAnnotation.typeAnnotation, nextDepth));
+				} else if (member.type === TSESTree.AST_NODE_TYPES.TSMethodSignature) {
+					score = addScore(score, calculateStructuralComplexity(member, nextDepth));
+				}
+			}
+			return score;
+		}
+
+		function calculateUnionComplexity(types: ReadonlyArray<TSESTree.TypeNode>, nextDepth: number): number {
+			let score = 0;
+			for (const type of types) score = addScore(score, calculateStructuralComplexity(type, nextDepth));
+			return addScore(score, 2 * (types.length - 1));
+		}
+
+		function calculateIntersectionComplexity(types: ReadonlyArray<TSESTree.TypeNode>, nextDepth: number): number {
+			let score = 0;
+			for (const type of types) score = addScore(score, calculateStructuralComplexity(type, nextDepth));
+			return addScore(score, 3 * types.length);
+		}
+
+		function calculateTupleComplexity(elementTypes: ReadonlyArray<TSESTree.TypeNode>, nextDepth: number): number {
+			let score = 1;
+			for (const element of elementTypes) {
+				if (
+					element.type !== TSESTree.AST_NODE_TYPES.TSRestType &&
+					element.type !== TSESTree.AST_NODE_TYPES.TSOptionalType
+				) {
+					score = addScore(score, calculateStructuralComplexity(element, nextDepth));
+				}
+			}
+			return addScore(score, 1.5 * elementTypes.length);
+		}
+
+		function calculateTypeReferenceComplexity(
+			typeArguments: TSESTree.TSTypeParameterInstantiation | undefined,
+			nextDepth: number,
+		): number {
+			let score = 2;
+			if (typeArguments === undefined) return score;
+
+			for (const parameter of typeArguments.params) {
+				score = addScore(score, calculateStructuralComplexity(parameter, nextDepth) + 2);
+			}
+			return score;
+		}
+
+		function calculateConditionalComplexity(node: TSESTree.TSConditionalType, nextDepth: number): number {
+			let score = 3;
+			score = addScore(score, calculateStructuralComplexity(node.checkType, nextDepth));
+			score = addScore(score, calculateStructuralComplexity(node.extendsType, nextDepth));
+			score = addScore(score, calculateStructuralComplexity(node.trueType, nextDepth));
+			return addScore(score, calculateStructuralComplexity(node.falseType, nextDepth));
+		}
+
+		function calculateFunctionTypeComplexity(
+			node: TSESTree.TSFunctionType | TSESTree.TSMethodSignature,
+			nextDepth: number,
+		): number {
+			let score = 2;
+			for (const parameter of node.params) {
+				const typeAnnotation = getParameterTypeAnnotation(parameter);
+				if (typeAnnotation !== undefined) {
+					score = addScore(score, calculateStructuralComplexity(typeAnnotation.typeAnnotation, nextDepth));
+				}
+			}
+			if (node.returnType !== undefined) {
+				score = addScore(score, calculateStructuralComplexity(node.returnType.typeAnnotation, nextDepth));
+			}
+			return score;
+		}
+
+		function calculateMappedTypeComplexity(node: TSESTree.TSMappedType, nextDepth: number): number {
+			let score = 5;
+			score = addScore(score, calculateStructuralComplexity(node.constraint, nextDepth));
+			if (node.typeAnnotation !== undefined) {
+				score = addScore(score, calculateStructuralComplexity(node.typeAnnotation, nextDepth));
+			}
+			return score;
+		}
+
 		function calculateStructuralComplexity(node: TSESTree.Node, depth = 0): number {
-			const cached = cache.nodeCache.get(node);
-			if (cached !== undefined) return cached;
-
-			if (cache.visitedNodes.has(node)) return 50;
-
-			cache.visitedNodes.add(node);
-
 			let score = 0;
 			const nextDepth = depth + 1;
 
@@ -210,55 +277,28 @@ const enforceIanitorCheckType = createRule<Options, MessageIds>({
 				case TSESTree.AST_NODE_TYPES.TSNeverKeyword:
 				case TSESTree.AST_NODE_TYPES.TSUnknownKeyword:
 				case TSESTree.AST_NODE_TYPES.TSAnyKeyword: {
-					score = 0;
 					break;
 				}
 
 				case TSESTree.AST_NODE_TYPES.TSInterfaceDeclaration: {
-					score = config.interfacePenalty;
-					const extendsLength = node.extends?.length;
-					if (extendsLength) score = addScore(score, extendsLength * 5);
-
-					const { body } = node.body;
-					score = addScore(score, body.length * 2);
-					for (const member of body) {
-						const typeAnnotation = "typeAnnotation" in member ? member.typeAnnotation : undefined;
-						if (typeAnnotation !== undefined) {
-							score = addScore(
-								score,
-								calculateStructuralComplexity(typeAnnotation.typeAnnotation, nextDepth),
-							);
-						}
-					}
+					const extendsLength = node.extends.length;
+					score = addScore(config.interfacePenalty, extendsLength * 5);
+					score = calculateTypeMembersComplexity(node.body.body, score, nextDepth);
 					break;
 				}
 
 				case TSESTree.AST_NODE_TYPES.TSTypeLiteral: {
-					const { members } = node;
-					score = 2 + members.length * 0.5;
-					for (const member of members) {
-						const typeAnnotation = "typeAnnotation" in member ? member.typeAnnotation : undefined;
-						if (typeAnnotation !== undefined) {
-							score = addScore(
-								score,
-								calculateStructuralComplexity(typeAnnotation.typeAnnotation, nextDepth),
-							);
-						}
-					}
+					score = calculateTypeMembersComplexity(node.members, 2 + node.members.length * 0.5, nextDepth);
 					break;
 				}
 
 				case TSESTree.AST_NODE_TYPES.TSUnionType: {
-					const { types } = node;
-					for (const type of types) score = addScore(score, calculateStructuralComplexity(type, nextDepth));
-					score = addScore(score, 2 * (types.length - 1));
+					score = calculateUnionComplexity(node.types, nextDepth);
 					break;
 				}
 
 				case TSESTree.AST_NODE_TYPES.TSIntersectionType: {
-					const { types } = node;
-					for (const type of types) score = addScore(score, calculateStructuralComplexity(type, nextDepth));
-					score = addScore(score, 3 * types.length);
+					score = calculateIntersectionComplexity(node.types, nextDepth);
 					break;
 				}
 
@@ -268,67 +308,28 @@ const enforceIanitorCheckType = createRule<Options, MessageIds>({
 				}
 
 				case TSESTree.AST_NODE_TYPES.TSTupleType: {
-					const { elementTypes } = node;
-					score = 1;
-					for (const element of elementTypes) {
-						const { type } = element;
-						if (type !== "TSRestType" && type !== "TSOptionalType") {
-							score = addScore(score, calculateStructuralComplexity(element, nextDepth));
-						}
-					}
-					score = addScore(score, 1.5 * elementTypes.length);
+					score = calculateTupleComplexity(node.elementTypes, nextDepth);
 					break;
 				}
 
 				case TSESTree.AST_NODE_TYPES.TSTypeReference: {
-					score = 2;
-					const { typeArguments } = node;
-					if (typeArguments !== undefined) {
-						for (const parameter of typeArguments.params) {
-							score = addScore(score, calculateStructuralComplexity(parameter, nextDepth) + 2);
-						}
-					}
+					score = calculateTypeReferenceComplexity(node.typeArguments, nextDepth);
 					break;
 				}
 
 				case TSESTree.AST_NODE_TYPES.TSConditionalType: {
-					score = 3;
-					score = addScore(score, calculateStructuralComplexity(node.checkType, nextDepth));
-					score = addScore(score, calculateStructuralComplexity(node.extendsType, nextDepth));
-					score = addScore(score, calculateStructuralComplexity(node.trueType, nextDepth));
-					score = addScore(score, calculateStructuralComplexity(node.falseType, nextDepth));
+					score = calculateConditionalComplexity(node, nextDepth);
 					break;
 				}
 
 				case TSESTree.AST_NODE_TYPES.TSMappedType: {
-					score = 5;
-					if ("constraint" in node) {
-						score = addScore(score, calculateStructuralComplexity(node.constraint, nextDepth));
-					}
-					if (node.typeAnnotation !== undefined) {
-						score = addScore(score, calculateStructuralComplexity(node.typeAnnotation, nextDepth));
-					}
+					score = calculateMappedTypeComplexity(node, nextDepth);
 					break;
 				}
 
 				case TSESTree.AST_NODE_TYPES.TSFunctionType:
 				case TSESTree.AST_NODE_TYPES.TSMethodSignature: {
-					score = 2;
-					for (const parameter of node.params) {
-						const typeAnnotation = "typeAnnotation" in parameter ? parameter.typeAnnotation : undefined;
-						if (typeAnnotation !== undefined) {
-							score = addScore(
-								score,
-								calculateStructuralComplexity(typeAnnotation.typeAnnotation, nextDepth),
-							);
-						}
-					}
-					if (node.returnType !== undefined) {
-						score = addScore(
-							score,
-							calculateStructuralComplexity(node.returnType.typeAnnotation, nextDepth),
-						);
-					}
+					score = calculateFunctionTypeComplexity(node, nextDepth);
 					break;
 				}
 
@@ -337,9 +338,6 @@ const enforceIanitorCheckType = createRule<Options, MessageIds>({
 			}
 
 			score *= getDepthMultiplier(depth);
-
-			cache.nodeCache.set(node, score);
-			cache.visitedNodes.delete(node);
 			return score;
 		}
 
@@ -392,9 +390,10 @@ const enforceIanitorCheckType = createRule<Options, MessageIds>({
 			},
 
 			VariableDeclarator(node): void {
+				if (node.id.type !== TSESTree.AST_NODE_TYPES.Identifier) return;
 				if (!node.init || node.init.type !== TSESTree.AST_NODE_TYPES.CallExpression) return;
 				if (!isIanitorValidator(node.init)) return;
-				if (hasTypeAnnotation(node)) return;
+				if (hasTypeAnnotation(node.id)) return;
 
 				const complexity = calculateIanitorComplexity(node.init);
 				if (complexity < config.baseThreshold) return;
@@ -403,8 +402,8 @@ const enforceIanitorCheckType = createRule<Options, MessageIds>({
 			},
 		};
 	},
-	defaultOptions: [DEFAULT_CONFIGURATION],
 	meta: {
+		defaultOptions: [DEFAULT_CONFIGURATION],
 		docs: {
 			description: "Enforce Ianitor.Check<T> type annotations on complex TypeScript types",
 		},

commit fef25c6ed1b30ae8d4859801ad2abe43be76c1e3

fix(enforce-ianitor-check-type): only report interfaces/type-aliases when Ianitor is referenced
Root cause: getDepthMultiplier at depth 0 changed from Math.log2(1)=0
to 1 via commit a2d8b708, which accidentally suppressed all interface
reporting before. The fix exposed a missing hasIanitorReference guard.

Fix: defer TSInterfaceDeclaration and TSTypeAliasDeclaration reporting
to Program:exit with an early return when hasIanitorReference is false.
Track Ianitor usage via Identifier visitor.

src/rules/enforce-ianitor-check-type.ts
@@ -153,6 +153,9 @@ const enforceIanitorCheckType = createRule<Options, MessageIds>({
 		const ianitorStaticVariables = new Set<string>();
 		const depthMultiplierCache = new Map<number, number>();
 		const complexityCeiling = config.errorThreshold * 2;
+		let hasIanitorReference = false;
+		const interfacesToCheck = new Map<TSESTree.TSInterfaceDeclaration, { complexity: number }>();
+		const typeAliasesToCheck = new Map<TSESTree.TSTypeAliasDeclaration, { complexity: number }>();
 
 		function getDepthMultiplier(depth: number): number {
 			const cached = depthMultiplierCache.get(depth);
@@ -344,7 +347,29 @@ const enforceIanitorCheckType = createRule<Options, MessageIds>({
 		const variableDeclaratorsToCheck = new Map<TSESTree.VariableDeclarator, { complexity: number }>();
 
 		return {
+			Identifier(node): void {
+				if (node.name === "Ianitor") hasIanitorReference = true;
+			},
+
 			"Program:exit"(): void {
+				if (!hasIanitorReference) return;
+
+				for (const [node, data] of typeAliasesToCheck.entries()) {
+					context.report({
+						data: { score: data.complexity.toFixed(1) },
+						messageId: "missingIanitorCheckType",
+						node,
+					});
+				}
+
+				for (const [node] of interfacesToCheck.entries()) {
+					context.report({
+						data: { name: node.id.name },
+						messageId: "complexInterfaceNeedsCheck",
+						node,
+					});
+				}
+
 				for (const [node, data] of variableDeclaratorsToCheck.entries()) {
 					if (
 						node.id.type === TSESTree.AST_NODE_TYPES.Identifier &&
@@ -363,30 +388,23 @@ const enforceIanitorCheckType = createRule<Options, MessageIds>({
 
 			TSInterfaceDeclaration(node): void {
 				const complexity = calculateStructuralComplexity(node);
-				const { name } = node.id;
+				if (complexity < config.interfacePenalty) return;
 
-				if (complexity >= config.interfacePenalty) {
-					context.report({
-						data: { name },
-						messageId: "complexInterfaceNeedsCheck",
-						node,
-					});
-				}
+				interfacesToCheck.set(node, { complexity });
 			},
 
 			TSTypeAliasDeclaration(node): void {
 				const variableName = extractIanitorStaticVariable(node.typeAnnotation);
-				if (variableName !== undefined) ianitorStaticVariables.add(variableName);
+				if (variableName !== undefined) {
+					hasIanitorReference = true;
+					ianitorStaticVariables.add(variableName);
+				}
 				if (hasIanitorStaticType(node.typeAnnotation)) return;
 
 				const complexity = calculateStructuralComplexity(node.typeAnnotation);
 				if (complexity < config.baseThreshold) return;
 
-				context.report({
-					data: { score: complexity.toFixed(1) },
-					messageId: "missingIanitorCheckType",
-					node,
-				});
+				typeAliasesToCheck.set(node, { complexity });
 			},
 
 			VariableDeclarator(node): void {

tests/rules/enforce-ianitor-check-type.test.ts
@@ -39,12 +39,16 @@ const isUser = Ianitor.strictInterface({
 				options: [{ baseThreshold: 1 }],
 			},
 			{
-				code: "type ComplexTuple = [{ a: string; b: number }, { c: boolean; d: string }];",
+				code: `
+const _ianitor = Ianitor.string;
+type ComplexTuple = [{ a: string; b: number }, { c: boolean; d: string }];
+`,
 				errors: [{ messageId: "missingIanitorCheckType" }],
 				options: [{ baseThreshold: 1, performanceMode: false }],
 			},
 			{
 				code: `
+const _ianitor = Ianitor.string;
 type ComplexAlias = {
     id: string;
     values: number[];
@@ -55,6 +59,7 @@ type ComplexAlias = {
 			},
 			{
 				code: `
+const _ianitor = Ianitor.string;
 interface ComplexService extends Base {
     config: {
         mode: string;